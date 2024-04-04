4a705f9d49d1cf2968819bb571212b2337b422af
Kelson Douglas <kelsondouglas23@hotmail.com>
2024-04-04 09:25:52 -0300

    feat: add get-events route

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import {
 import fastify from "fastify";
 import { createEvent } from "./routes/create-event";
 import { registerForEvent } from "./routes/register-for-event";
+import { getEvent } from "./routes/get-event";
 
 export const app = fastify();
 app.setValidatorCompiler(validatorCompiler);
@@ -12,6 +13,7 @@ app.setSerializerCompiler(serializerCompiler);
 
 app.register(createEvent);
 app.register(registerForEvent);
+app.register(getEvent);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("Server running on port 3333");
